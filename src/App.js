7f2918e2f3094d7d2e2f5abc15e56d660bafe1c6
Alex Bukhman <alexb823@gmail.com>
2020-03-01 10:18:46 -0500

    render and increment counter tests working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
   render() {
-    return <div data-test="component-app"></div>;
+    const { counter } = this.state;
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently {counter}</h1>
+        <button data-test="increment-button" onClick={this.increment}>
+          Increment Counter
+        </button>
+      </div>
+    );
   }
 }
 
